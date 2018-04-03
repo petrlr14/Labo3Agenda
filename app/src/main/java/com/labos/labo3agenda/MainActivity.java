d5b0ae870e29018f5e94ebd883e554f7b47d78d5
00017316 <00017316@uca.edu.sv>
2018-04-03 12:06:37 -0600

    first commit

---
+++ app/src/main/java/com/labos/labo3agenda/MainActivity.java
@@ -0,0 +1,13 @@
+package com.labos.labo3agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
